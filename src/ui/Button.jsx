b68991a240259d5e1a92a4d9d89d4284506ef494
Lê Sĩ Toàn <127379810+lesitoan@users.noreply.github.com>
2025-01-03 21:14:38 +0700

    Update Button.jsx

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,7 @@
 const Button = ({ children, onClick, disable }) => {
     return (
         <button
+            
             className={`${
                 disable && 'cursor-not-allowed'
             } py-2 px-10 bg-main-color text-[#fff] font-[500] text-[18px] hover:bg-main-color-90 rounded-sm transition duration-300 ease-in-out`}
